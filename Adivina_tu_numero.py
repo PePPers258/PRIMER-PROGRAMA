4fa24e4c34ae23fb61bfe9a70b741a566d61efa6
PePPers258 <sajimenez@gmail.com>
2020-01-13 19:09:28 -0600

    TODO LO NUEVO

--- Adivina_tu_numero.py
+++ Adivina_tu_numero.py
@@ -3,8 +3,7 @@ number_to_guess = int(input("Para continuar, introduce un numero para que alguie
 
 user_number = int(input("Adivina un numero: "))
 
-while number_to_guess < 0 or number_to_guess > 0:
+while number_to_guess < user_number or number_to_guess > user_number:
     print("Has fallado, intentalo de nuevo")
     user_number = int(input("Adivina un numero: "))
-
 print("¡felicidades, adivinaste el nuemro, el numero era {}" .format(number_to_guess))
